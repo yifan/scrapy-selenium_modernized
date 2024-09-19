124670b81561d3b8d7e5cfdfb224ce6763b42516
Yifan Zhang <yifan2019@icloud.com>
2024-09-19 15:59:51 +0300

    rewrite without pip internal

--- setup.py
+++ setup.py
@@ -1,30 +1,18 @@
 """This module contains the packaging routine for the pybook package"""
 
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
-
 
 def get_requirements(source):
     """Get the requirements from the given ``source``
 
     Parameters
     ----------
-    source: str
+    source: stlarger
         The filename containing the requirements
 
     """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+    with open(source) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
     packages=find_packages(),
